c4a0c8ef1e6e07aeeaedd65d4089cc00287a209b
leonardorodriguesflar <leonardo.rodrigues@flarconsulting.com.br>
2025-07-30 11:08:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/propostatorra/", //
   server: {
     host: "::",
     port: 8080,
@@ -20,3 +20,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
